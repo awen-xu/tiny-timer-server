b7a37cd4986afed212f55c17e7edb3ef5e9f788f
Kevin Xu <kevinxu.444@gmail.com>
2018-08-20 12:14:51 -0400

    Changed server to HTTPS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 const path = require('path');
-
 
 // Routers
 const loginRouter = require('./routes/login');
@@ -17,8 +18,9 @@ app.get('/', (req, res, next) => {
 app.use('/login', loginRouter);
 app.use('/registration', registrationRouter);
 
-app.listen(8080, () => {
-	console.log('Listening on port 8080');
-});
+https.createServer({
+	key: fs.readFileSync('./ssl/key.pem'),
+	cert: fs.readFileSync('./ssl/cert.pem')
+}, app).listen(8080);
 
 module.exports = app;
